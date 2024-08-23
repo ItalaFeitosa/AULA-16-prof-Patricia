9af77ff14a5ab9155a953e0fd8d7584157f5e55b
ItalaFeitosa <itala.feitosa@etec.sp.gov.br>
2024-08-23 14:20:11 -0300

    arquivo do Git Bash

---
+++ NetBeansProjects/JavaApplication1/src/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Etec-User
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "Hello Java!!!");
+    }
+    
+}
